e1c7c49c119fbaa432e64db7d0a0e94dcfcf20d2
Mudasir <apple.mudasir@gmail.com>
2026-06-29 21:23:58 +0700

    Auto commit

--- src/db/index.js
+++ src/db/index.js
@@ -13,7 +13,7 @@ try { /* Try connecting to the database. If anything fails, execution jumps to t
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(`The DB is Connected and Host Name is ${connectionInstance.connection.host}`);
 
-        /*. The DB is Connected and Host Name is localhost.  /
+        /*. The DB is Connected and Host Name is localhost.  */
         
     /* await tells JavaScript: Wait until the database is connected before continuing.  */
 
