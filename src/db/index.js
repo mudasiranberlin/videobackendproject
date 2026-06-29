68a8b3f73b8f3691f357334e17ac43b9ec4df57d
Mudasir <apple.mudasir@gmail.com>
2026-06-29 19:47:31 +0700

    Auto commit

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,7 @@ import { DB_NAME } from "../constants";
 
 const connectionInstance = async ()=>{
     try {
-        
+        await 
         
     } catch (error) {
         console.log("The error connecting the database")
