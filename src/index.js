ffc6608ea68f2bfa629dbbf60a3bd628da5a3732
Mudasir <apple.mudasir@gmail.com>
2026-06-29 19:33:42 +0700

    Auto commit

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const app = express();
         })
         
         app.listen(process.env.PORT,()=>{
-            console.log(`APP is Listening `,${process.ev});
+            console.log(`APP is Listening `,${process.env.PORT});
             
         })
 
