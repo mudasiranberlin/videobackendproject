87bbd85c25f44ae578e5ae5024c89e665ea67922
Mudasir <apple.mudasir@gmail.com>
2026-06-29 19:51:24 +0700

    Auto commit

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ import { DB_NAME } from "../constants";
 const  connectDB= async ()=>{
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log(`The DB is Connected and Host Name is ${co}`);
+        console.log(`The DB is Connected and Host Name is ${connectionInstance.}`);
         
         
     } catch (error) {
