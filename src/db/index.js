497096404d098b42fa91be8d0bdd3837d28173c8
Mudasir <apple.mudasir@gmail.com>
2026-06-29 20:03:00 +0700

    Auto commit

--- src/db/index.js
+++ src/db/index.js
@@ -9,11 +9,11 @@ const  connectDB= async ()=>{
 /* You're creating a function named connectDB. It is not running yet.It only runs when you call:connectDB(); */
 
 try { /* Try connecting to the database. If anything fails, execution jumps to the catch block. */
-    
+
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(`The DB is Connected and Host Name is ${connectionInstance.connection.host}`);
         
-        
+      
     } catch (error) {
         console.log("The error connecting the database")
         process.exit(1)
