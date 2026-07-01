1aa3664d45df09f9cfd30852d6ffd74377446b67
Mudasir <apple.mudasir@gmail.com>
2026-07-01 19:15:48 +0700

    Auto commit

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ dotenv.config({
 connectDB()  //A function that connects to MongoDB.
 .then(()=>{ // Since connecting takes time, connectDB() returns a Promise.
 // A Promise has two possibilities: Success ✅ or Failure ❌  
+// So we use:
     app.on("error",(error)=>{     // app → our Express application also our website/server
         console.log("ERRR:",error);
         throw error
