dbf29619bebbd95686988374f5700606a91cf30d
Mudasir <apple.mudasir@gmail.com>
2026-07-01 17:30:08 +0700

    Auto commit

--- src/db/index.js
+++ src/db/index.js
@@ -23,7 +23,7 @@ try { /* Try connecting to the database. If anything fails, execution jumps to t
     MongoDB not running
     Wrong URL this block runs.
 */
-        console.log("The error connecting the database")
+        console.log("")
         process.exit(1)      /*Stop the application because something serious went wrong.*/
     } 
 
