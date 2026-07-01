b59148a94bccee0797da09c79381bac9d35212d8
Mudasir <apple.mudasir@gmail.com>
2026-07-01 19:15:31 +0700

    Auto commit

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ dotenv.config({
 
 connectDB()  //A function that connects to MongoDB.
 .then(()=>{ // Since connecting takes time, connectDB() returns a Promise.
-// A Promise has two possibilities: Success ✅ orFailure ❌
+// A Promise has two possibilities: Success ✅ or Failure ❌
     app.on("error",(error)=>{     // app → our Express application also our website/server
         console.log("ERRR:",error);
         throw error
