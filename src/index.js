044ec48556d9cd33e0d1a505972ea76708c56e4e
Mudasir <apple.mudasir@gmail.com>
2026-07-01 17:43:01 +0700

    Auto commit

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ dotenv.config({
 connectDB()
 .then(()=>{
     app.on("error",(error)=>{
-        console.log("");
+        console.log("ERR");
         
     })
     app.listen(process.env.PORT||8080,()=>{
