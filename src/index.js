8bec86ef8c17f0a2f78b215ac6c83c45a791ce0d
Mudasir <apple.mudasir@gmail.com>
2026-07-01 17:40:17 +0700

    Auto commit

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ dotenv.config({
 connectDB()
 .then(()=>{
     app.listen(process.env.PORT||8080,()=>{
-        console.log(`Server is Running at Port : ${process.env}`);
+        console.log(`Server is Running at Port : ${process.env.}`);
         
     })
 })
