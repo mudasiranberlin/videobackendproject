c40315905eef4fb79e69070c39a9b97543b82729
Mudasir <apple.mudasir@gmail.com>
2026-07-01 19:20:35 +0700

    Auto commit

--- src/index.js
+++ src/index.js
@@ -21,8 +21,7 @@ This means:
 "If MongoDB connects successfully, do this."
 */
 .then(()=>{ 
-    app.on("error",(error)=>{     /*
-    Listen for server errors
+    app.on("error",(error)=>{     /* Listen for server errors
     */
         console.log("ERRR:",error);
         throw error.  
