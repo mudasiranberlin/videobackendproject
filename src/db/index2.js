13c3d4c231e5f19480321b2aecdd815528d5e3fa
Mudasir <apple.mudasir@gmail.com>
2026-07-01 17:02:34 +0700

    Auto commit

--- src/db/index2.js
+++ src/db/index2.js
@@ -5,7 +5,7 @@ const ConnectDB = async () => {
     try {
         
     } catch (error) {
-        console.log(`Error while `);
+        console.log(`Error while Connecting mongodb`);
         
     }
 } 
