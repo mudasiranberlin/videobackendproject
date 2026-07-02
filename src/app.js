6a7439107eae4e5a1ecaceeb5e50fd21a9fbc996
Mudasir <apple.mudasir@gmail.com>
2026-07-02 21:50:53 +0700

    Auto commit

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(express.json({limit:"16kb"})) // submits a form written in JSON.express.
  "age":10
 }
 
-and turns it into req.body
+and turns it into:  req.body
 
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 
