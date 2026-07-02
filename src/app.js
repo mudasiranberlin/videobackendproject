c555fa6dda88f1000403bf3d3b7a25b29db24f0b
Mudasir <apple.mudasir@gmail.com>
2026-07-02 21:51:30 +0700

    Auto commit

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(cors({
 
 app.use(express.json({limit:"16kb"})) 
 
-// submits a form written in JSON.express.json() works like a translator.
+ submits a form written in JSON.express.json() works like a translator.
 // {
  "name":"Ram",
  "age":10
