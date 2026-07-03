71464d8c415c5fb3a41d39ec49854e239b352780
Mudasir <apple.mudasir@gmail.com>
2026-07-03 20:20:41 +0700

    Auto commit

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -6,7 +6,7 @@ async (req,res,next) => {
     catch (error) {
         res.status(error.code || 5000).json({
             success:false,
-            
+            message
         })
         
     }
