986a68462f8aa84112b2e5d427ca369e4a17a720
Mudasir <apple.mudasir@gmail.com>
2026-07-03 20:45:26 +0700

    Auto commit

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -10,7 +10,8 @@ class ApiError extends Error {
         this.statusCode = statusCode,
         this.data = null,
         this.message =message,
-        this.success =false
+        this.success =false,
+        
         
     }
 }
