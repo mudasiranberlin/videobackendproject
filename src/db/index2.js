b87acbfe47c91852ae04c43d854029836aff190e
Mudasir <apple.mudasir@gmail.com>
2026-07-04 16:34:08 +0700

    Auto commit

--- src/db/index2.js
+++ src/db/index2.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 connectDB = async () => {
     try {
-        connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`);
+        connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
         
     } catch (error) {
         console.log("Erroe cOnnecting database",error);
