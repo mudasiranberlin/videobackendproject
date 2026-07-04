eff2a6edade584053f5ebbde60537175eb63cc66
Mudasir <apple.mudasir@gmail.com>
2026-07-04 16:54:52 +0700

    Auto commit

--- src/db/index2.js
+++ src/db/index2.js
@@ -5,7 +5,7 @@ connectDB = async () => {
         connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
         console.log(`The database has been connected sucessfully|| at port ${connectionInstance.connection.host} `)
     } catch (error) {
-        console.log("Erroe connecting database",error);
+        console.log("Erro connecting database",error);
         
     }
 }
