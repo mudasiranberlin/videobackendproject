13a7063d3eb04ee5e28837b8181f97b2ab7b5efa
Mudasir <apple.mudasir@gmail.com>
2026-07-06 20:24:52 +0700

    Auto commit

--- src/db/index2.js
+++ src/db/index2.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const connectionInstance = 
+        const connectionInstance = await mongoose.connect
         
     } catch (error) {
         console.log("Failed to COnnect database|| !!!");
