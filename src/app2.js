3081b46bd5f601b6af7977c7e1d6a254c301157c
Mudasir <apple.mudasir@gmail.com>
2026-07-06 20:33:41 +0700

    Auto commit

--- src/app2.js
+++ src/app2.js
@@ -1,14 +1,1 @@
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
-
-app.use(cors({
-    origin:process.env.CORS_ORIGIN,
-    credentials:true
-}))
-
-app.use(cookieParser())
-app.use(express.json({limit:"16kb"}));
-app.use(express.urlencoded({extends: true, limit:"16kb"}))
-
-app.use(express.static("public"))
+import mongoose from "mongoose";
