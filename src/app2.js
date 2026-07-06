3fe5c80bca374880380e0817505d73893ed5c64f
Mudasir <apple.mudasir@gmail.com>
2026-07-06 21:01:21 +0700

    Auto commit

--- src/app2.js
+++ src/app2.js
@@ -12,7 +12,7 @@ app.use(cors({
 
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({limit:"16kb"}))
-app.use(express.static())
+app.use(express.static("public"))
 app.use(cookieParser())
 
 
