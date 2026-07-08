c6e6380cff5f7561aac085258ce04bc3d04cfd27
Mudasir <apple.mudasir@gmail.com>
2026-07-08 11:30:47 +0700

    Auto commit

--- src/index.js
+++ src/index.js
@@ -42,6 +42,7 @@ const app = express()
 
 connectDB()
 .then(()=>{
+    app.on
      app.listen(process.env.PORT||8080,()=>{
         console.log(`The database has been connected sucessfully at port || ${process.env.PORT}`);
         
