3c4ff807d91def8fbbc67af4990a08054af54266
Mudasir <apple.mudasir@gmail.com>
2026-07-08 13:23:50 +0700

    Auto commit

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,4 @@
-import mongoose, {} from "mongoose";
+import mongoose, {Schema} from "mongoose";
 const Userschema = new mongoose.Schema({},{timestamps:true})
 
 export const User = mongoose.model("User",Userschema) 
