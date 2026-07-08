f2bfd209a5ebc4b3842e755a7a77004b6bb31539
Mudasir <apple.mudasir@gmail.com>
2026-07-08 11:48:43 +0700

    Auto commit

--- src/app2.js
+++ src/app2.js
@@ -1,2 +1,7 @@
 import mongoose from "mongoose";
 import cors from 
+
+var corsOptions = {
+  origin: 'http://example.com',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
