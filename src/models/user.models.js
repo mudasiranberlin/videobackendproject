d61314519a4717e9a20058a2a63d91afd9de9f39
Mudasir <apple.mudasir@gmail.com>
2026-07-08 14:25:32 +0700

    Auto commit

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -64,7 +64,7 @@ Userschema.methods.generateAccessToken = function () {
         username:this.username,
         fullname:this.fullname // this one is comming from database this.fullname
     },{
-        process.env.ACCESS
+        process.env.ACCESS_
     })
     
 } 
