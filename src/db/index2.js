2862337e7fe498ce6343eb544ffb6fab5bdfe910
Mudasir <apple.mudasir@gmail.com>
2026-07-08 11:15:12 +0700

    Auto commit

--- src/db/index2.js
+++ src/db/index2.js
@@ -8,7 +8,7 @@ const app = express()
     async()=>{
         try {
             await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-            console.log(`Mo`);
+            console.log(`Mongo db connected`);
             
         } catch (error) {
             
