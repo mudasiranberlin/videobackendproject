fe0e6c8fcd1b9857a279f78a5e435671a5337174
Mudasir <apple.mudasir@gmail.com>
2026-07-08 13:44:49 +0700

    Auto commit

--- src/models/video.models.js
+++ src/models/video.models.js
@@ -2,8 +2,9 @@ import mongoose,{Schema} from "mongoose";
 
 const VideoSchema = new Schema({
     videoFile:{
-        
-    }
+
+    },
+    thum
 },{timestamps:true})
 
 export const Video = mongoose.model("Video",VideoSchema)
