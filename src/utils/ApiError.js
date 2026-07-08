7c23804888ccde8e73ba968d666cd1edc73104c7
Mudasir <apple.mudasir@gmail.com>
2026-07-08 13:13:55 +0700

    Auto commit

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,27 +1,4 @@
-class ApiError extends Error {
-    constructor(
-        statusCode,
-        message :"Something Went Wrong",
-        error = [],
-        stack = ""
 
-    ){
-        super(message),
-        this.statusCode = statusCode,
-        this.data = null,
-        this.message = message,
-        this.success = false,
-        this.errors =error
-
-        if (stack) {
-            this.stack =stack
-        }
-        else{
-            Error.captureStackTrace(this,this.constructor)
-        }
-        
-    }
-}
 class ApiError extends Error {
     constructor(
         statusCode,
