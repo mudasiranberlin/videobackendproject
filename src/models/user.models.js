540d4b9320579a2253bd323b525f39df585c0db7
Mudasir <apple.mudasir@gmail.com>
2026-07-08 14:11:39 +0700

    Auto commit

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -47,6 +47,6 @@ Userschema.pre('save', async function (next) {
     next()
 } )
 
-Userschema.methods.isPassword
+Userschema.methods.isPasswordCorrect
 
 export const User = mongoose.model("User",Userschema) 
