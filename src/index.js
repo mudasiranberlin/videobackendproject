d36f93d1bd452a2e209571f168b0ff08321c610f
Mudasir <apple.mudasir@gmail.com>
2026-07-08 11:28:59 +0700

    Auto commit

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ const app = express()
 
 connectDB()
 .then(
-    app.listen(process)
+    app.listen(process.env.)
 )
 .catch( (error)=>{
     console.log(`Failed to connect with the database !!!`,error);
