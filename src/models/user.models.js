98403beb56b17ffcdfd32a2e1b0d86b4eccb29e8
Mudasir <apple.mudasir@gmail.com>
2026-07-08 14:10:12 +0700

    Auto commit

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -42,7 +42,7 @@ const Userschema = new Schema({
 },{timestamps:true})
 
 Userschema.pre('save', async function (next) {
-    if(this.Mo)
+    if(this.Mod)
     this.password = bcrypt.hash(this.password,10)
     next()
 } )
