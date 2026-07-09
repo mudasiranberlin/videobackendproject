dd4bdfdebfe62e574378c2b4b7ada7c2680e6504
Mudasir <apple.mudasir@gmail.com>
2026-07-09 12:04:56 +0700

    Auto commit

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -122,7 +122,7 @@ Userschema.methods.generateRefreshToken( function () {
         },
         process.env.REFRESH_TOKEN_SECRET
         {
-            expiresIn:process.env.as
+            expiresIn:process.env.ASSESS_TOKEN_EXPIRY
         }
     )
     
