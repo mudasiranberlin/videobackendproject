3a16c657fc86f6a784e73e542ab303d3c95c64fd
Mudasir <apple.mudasir@gmail.com>
2026-07-09 15:01:06 +0700

    Auto commit

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,27 +1,12 @@
-// Import multer library
-// Multer helps us handle file uploads from users
 import multer from "multer";
 
-// Create storage settings for uploaded files
 const storage = multer.diskStorage({
-  // Decide where to save the uploaded file temporarily
   destination: function (req, file, cb) {
-    // cb means callback function
-    // First value = error (null means no error)
-    // Second value = folder location where file will be saved
     cb(null, '/public/temp')
   },
-  // Decide the name of the uploaded file
   filename: function (req, file, cb) {
-
-    // Save file using its original name
-    // Example:
-    // User uploads: profile.jpg
-    // Saved as: profile.jpg
     cb(null, file.originalname)
   }
-
 })
-// Create multer upload middleware
-// We export it so we can use it in routes
+
 export const upload = multer({ storage })
