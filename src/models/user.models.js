d71932075dedb5f37e090996b53d858e4d75ac95
Mudasir <apple.mudasir@gmail.com>
2026-07-09 11:55:19 +0700

    Auto commit

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -95,6 +95,6 @@ Userschema.pre("save",async function (next) {
     next()
 })
 Userschema.methods.isPasswordCorrect = async function (password) {
-    bcrypt.compare(password)
+    bcrypt.compare(password,this)
     
 }
