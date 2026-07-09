68f1f32eb5e5014569a2eeb2f112bfa7578bff9d
Mudasir <apple.mudasir@gmail.com>
2026-07-09 12:56:49 +0700

    Auto commit

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -2,7 +2,7 @@ import {v2 as cloudinary} from 'cloudinary'
 import fs from "fs"
 
 cloudinary.config({
-    cloud_name: "your_cloud_name",
+    cloud_name: PR
     api_key: "your_api_key",
     api_secret: "your_api_secret"
 });
