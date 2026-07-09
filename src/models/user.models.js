dd154cc618ad5e17a61a6835b4a379775118ae6e
Mudasir <apple.mudasir@gmail.com>
2026-07-09 11:53:26 +0700

    Auto commit

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -90,7 +90,7 @@ import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
 
 Userschema.pre("save",async function (next) {
-    if(!this.isModified("password")) return
+    if(!this.isModified("password")) return next
     this.password = bcrypt.hash(this.password,10)
     next()
     
