31cfcea2af1d0850810746ea8bacaccafb647c23
Mudasir <apple.mudasir@gmail.com>
2026-07-09 14:45:05 +0700

    Auto commit

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -11,6 +11,7 @@ cloudinary.config({
 const uploadCloudinary = async (localFilePath)=>{
     try {
         if (!localFilePath) return null
+        // upload the file on cloud nary 
         
     } catch (error) {
         
