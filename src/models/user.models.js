56cbc7a32f26d0f66fdaf3b4057f794603e6335b
Mudasir <apple.mudasir@gmail.com>
2026-07-09 12:01:15 +0700

    Auto commit

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -100,7 +100,7 @@ Userschema.methods.isPasswordCorrect = async function (password) {
 }
 
 Userschema.methods.generateAccessToken( function () {
-    jwt.sign
+    jwt.sign()
     
 });
 Userschema.methods.generateRefreshToken( function () {
