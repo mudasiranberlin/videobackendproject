24b8ecaffb5309d251805261939e7020f56ea8ec
Mudasir <apple.mudasir@gmail.com>
2026-07-10 13:46:08 +0700

    Auto commit

--- src/models/usermodel.js
+++ src/models/usermodel.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 import bycrpt from "bcrypt"
 import jwt from "jsonwebtoken"
 
-const Userschema = new mongoose.Schema({},{timestamps:true})
+const Userschema = new mongoose.Schema({
+    
+},{timestamps:true})
 
 export const User = mongoose.model("User",Userschema)
