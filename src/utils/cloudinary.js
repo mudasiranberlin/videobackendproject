7bd845e1eb3f9ed4054529e5ff6e8078be3f7e02
Mudasir <apple.mudasir@gmail.com>
2026-07-10 14:29:28 +0700

    Auto commit

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -72,32 +72,3 @@ export { uploadCloudinary }
 
 
 
-import { v2 as cloudinary } from 'cloudinary';
-
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-  });
-
-  const uploadCloudinary = async (localFilePath) => {
-    try {
-        if (!localFilePath) return null
-
-        // upload the file on cloudnary 
-       const response= await cloudinary.uploader.upload(localFilePath,{
-            resource_type:"auto"
-        })
-        console.log("File has been upload sucessfully",response.url);
-        return response;
-        
-
-        
-    } catch (error) {
-        fs.unlinkSync(localFilePath)
-        return null
-        console.log("Error Uploading file on cloud nary ");
-        
-        
-    }
-  } 
