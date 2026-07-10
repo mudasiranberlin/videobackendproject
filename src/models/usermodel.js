f100de74222c91dec98559c38499e9584839ac62
Mudasir <apple.mudasir@gmail.com>
2026-07-10 14:04:18 +0700

    Auto commit

--- src/models/usermodel.js
+++ src/models/usermodel.js
@@ -16,7 +16,7 @@ const Userschema = new mongoose.Schema({
 Userschema.pre('save', async function(next) {
     if (this.isModified("password"))  return next()
      this.password= await bcrypt.hash(this.password, 10) 
-    return 
+    return next()
 });
 
 Userschema.methods.generateaccesstoken = function () {
