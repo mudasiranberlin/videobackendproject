0352bfecc5da95a6372e3bf47ca113f4a4a3c0f2
Mudasir <apple.mudasir@gmail.com>
2026-07-10 14:25:57 +0700

    Auto commit

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -88,7 +88,7 @@ cloudinary.config({
         cloudinary.uploader.upload(localFilePath,{
             resource_type:"auto"
         })
-        console.log("File has been upload");
+        console.log("File has been upload sucessfully");
         
 
         
