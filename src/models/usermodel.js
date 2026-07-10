46abba9fc2e3a52d476405537580e5a298112ad4
Mudasir <apple.mudasir@gmail.com>
2026-07-10 13:59:40 +0700

    Auto commit

--- src/models/usermodel.js
+++ src/models/usermodel.js
@@ -28,7 +28,7 @@ Userschema.methods.generateaccesstoken = function () {
 }
 
 Userschema.methods.accessstoken = function () {
-    
+    return jwt
 }
 
 export const User = mongoose.model("User",Userschema)
