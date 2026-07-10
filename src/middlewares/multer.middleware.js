14874fb7aa03534afd0e93873682a3fe736272dc
Mudasir <apple.mudasir@gmail.com>
2026-07-10 14:33:15 +0700

    Auto commit

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -36,7 +36,14 @@ export const upload = multer({ storage })
 
 import multer from "multer";
 
-app.post('/profile', upload.single('avatar'), function (req, res, next) {
-  // req.file is the `avatar` file
-  // req.body will hold the text fields, if there were any
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '/tmp/my-uploads')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.fieldname + '-' + uniqueSuffix)
+  }
 })
+
+const upload = multer({ storage: storage })
