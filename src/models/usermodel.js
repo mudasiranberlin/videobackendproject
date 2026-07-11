76c7c62113d5f835adda762fda567477dbf20153
Mudasir <apple.mudasir@gmail.com>
2026-07-11 12:08:29 +0700

    Auto commit

--- src/models/usermodel.js
+++ src/models/usermodel.js
@@ -51,7 +51,8 @@ const Userschema = new mongoose.Schema({
     }
     Userschema.methods.createrefreshtoken = function () {
         return jwt.sign({ 
-            _id=this.
+            _id=this._id,
+            
 
          });
         
