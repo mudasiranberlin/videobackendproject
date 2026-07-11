da7a371a74debabf0fbefb2dc6ccfccc269bacd9
Mudasir <apple.mudasir@gmail.com>
2026-07-11 12:06:53 +0700

    Auto commit

--- src/models/usermodel.js
+++ src/models/usermodel.js
@@ -49,7 +49,7 @@ const Userschema = new mongoose.Schema({
         return await bycrpt.compare(password,this.password)
         
     }
-    Userschema.methods
+    Userschema.methods.
 
 
 export const User = mongoose.model("User",Userschema);
