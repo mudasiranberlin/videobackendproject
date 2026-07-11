5904a14123b1d5bed2b75f2b6cdc0f59776afc54
Mudasir <apple.mudasir@gmail.com>
2026-07-11 11:54:10 +0700

    Auto commit

--- src/models/usermodel.js
+++ src/models/usermodel.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import bycrpt from "bcrypt"
 import jwt from "jsonwebtoken"
-import mongoose from "mongoose-";
+import mongoose from "mongoose-a";
 
 const Userschema = new mongoose.Schema({
 
