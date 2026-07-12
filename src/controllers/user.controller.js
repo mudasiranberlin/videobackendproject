95f64eea286983fd0691fc688783109efa4e498a
Mudasir <apple.mudasir@gmail.com>
2026-07-12 13:56:05 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -28,7 +28,9 @@ const registerUser = asyncHandler( async (req,res)=>{
    const avatarlocalpath =  req.files?.avatar[0]?.path;
    const coverimagelocalpath =req.files?.coverImage[0]?.path;
 
-   if
+   if (condition) {
+    
+   }
     
 } )
 
