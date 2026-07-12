857357fbe37d2d1e73c42e57417db3d0b14d4fa5
Mudasir <apple.mudasir@gmail.com>
2026-07-12 13:22:40 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,8 @@
 import { asyncHandler } from "../utils/asyncHandler.js"
 
 const registerUser = asyncHandler( async (req,res)=>{
-    res.status(200).json({
-        message:"ok mudasir "
+    // res.status(200).json({
+    //     message:"ok mudasir "
     })
 } )
 
