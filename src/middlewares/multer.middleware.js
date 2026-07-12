021c7062158fcacf5d92398dc747516999c491a4
Mudasir <apple.mudasir@gmail.com>
2026-07-12 13:32:33 +0700

    Auto commit

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -34,28 +34,3 @@ export const upload = multer({ storage })
 
 
 
-import multer from "multer";
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, '/public/temp')
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname)
-  }
-})
-
-const upload = multer({ storage: storage })
-
-
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, '/tmp/my-uploads')
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname)
-  }
-})
-
-export const upload = multer({ storage: storage })
