3acff10fe4b184059984bfc390a3f3c259aec8c3
Mudasir <apple.mudasir@gmail.com>
2026-07-12 10:57:54 +0700

    Auto commit

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -70,3 +70,14 @@ const uploadCloudinary = async (localFilePath) => {
 export { uploadCloudinary }
 
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '/tmp/my-uploads')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.fieldname + '-' + uniqueSuffix)
+  }
+})
+
+const upload = multer({ storage: storage })
