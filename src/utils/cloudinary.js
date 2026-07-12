60ccc3efb90603f53234f0224471d415269a0c4f
Mudasir <apple.mudasir@gmail.com>
2026-07-12 11:06:07 +0700

    Auto commit

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -80,7 +80,7 @@ const uploadFile = async function (localFilePath) {
     try {
         if (!localFilePath) return null
         const response = await cloudinary.uploader.upload(file, { folder: 'uploads' });
-    console.log("File uploaded successfully:", result.url);
+    console.log("File uploaded successfully:", res.url);
     return result.url;
         
     } catch (error) {
