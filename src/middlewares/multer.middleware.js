129642c787e3e3b936b3322380fbac1724ecb472
Mudasir <apple.mudasir@gmail.com>
2026-07-12 11:12:24 +0700

    Auto commit

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -54,7 +54,6 @@ const storage = multer.diskStorage({
     cb(null, '/tmp/my-uploads')
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
     cb(null, file.fieldname + '-' + uniqueSuffix)
   }
 })
