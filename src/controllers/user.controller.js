769591879b99e55f1bbe00ef5372450370cdd864
Mudasir <apple.mudasir@gmail.com>
2026-07-12 13:38:34 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -8,7 +8,7 @@ const registerUser = asyncHandler( async (req,res)=>{
     const {fullname,username,email} = req.body
     console.log(email,fullname);
     if (fullname=="") {
-        throw new ApiError(400,"Full name")
+        throw new ApiError(400,"Full Name is required")
     }
     
 } )
