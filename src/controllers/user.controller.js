bc22ee18d6845ae96e5714484d33fb473afb4e28
Mudasir <apple.mudasir@gmail.com>
2026-07-12 13:46:59 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -13,8 +13,7 @@ const registerUser = asyncHandler( async (req,res)=>{
     // }
     if(
         [fullname,email,username,password].some((field)=>
-            field?.trim() === "")
-        })
+            field?.trim() === ""))
     )
     
 } )
