810f38d1da7cc0270a8e2ef0abe1778294cd55e7
Mudasir <apple.mudasir@gmail.com>
2026-07-13 12:27:55 +0700

    Auto commit

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -82,7 +82,7 @@ class ApiError extends Error {
       this.stack =stack
     }
     else{
-      Error.captureStackTrace(this,this)
+      Error.captureStackTrace(this,this.constructor)
     }
   }
 }
