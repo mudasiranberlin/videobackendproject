0619df3a33815f4776dfcdbbbde306bd899a3f91
Mudasir <apple.mudasir@gmail.com>
2026-07-13 12:13:48 +0700

    Auto commit

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -8,7 +8,7 @@ const asyncHandler = (requestHandler)=>{
 export {asyncHandler}
 
 const asyncHandler = (requestHandler)=>{
-    ()=>{
+    (req)=>{
         Promise.resolve()
         .catch((error)=>next(error))
     }
