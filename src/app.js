07eb4b4d99cc897176ee7a0b9e8c29789e67b4df
Mudasir <apple.mudasir@gmail.com>
2026-07-13 14:45:19 +0700

    Auto commit

--- src/app.js
+++ src/app.js
@@ -47,6 +47,6 @@ app.use(cookieParser()) // cookieParser() reads those cookies.
 
 import userRouter from './routes/user.routes.js'
 
-app.use("/api/v1/users",userRouter)res
+app.use("/api/v1/users",userRouter)
 
 export default app
