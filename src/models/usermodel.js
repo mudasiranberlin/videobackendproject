448e7619ea95bb69cccbfd834ead43bc9d3067f4
Mudasir <apple.mudasir@gmail.com>
2026-07-13 13:19:28 +0700

    Auto commit

--- src/models/usermodel.js
+++ src/models/usermodel.js
@@ -31,7 +31,7 @@ const UserSchema = new Schema({
 
 
 UserSchema.schema.pre('save', function() {
-    if (this.isModified ="password") return next
+    if (this.isModified ="password") return next()
     }
      this.password = await  bcrypt.hash(this.password,10)
 });
