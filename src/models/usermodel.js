01e3f79ad79c6112e457e8cd9131adf502951cef
Mudasir <apple.mudasir@gmail.com>
2026-07-13 13:24:18 +0700

    Auto commit

--- src/models/usermodel.js
+++ src/models/usermodel.js
@@ -39,5 +39,5 @@ UserSchema.schema.pre('save', function() {
 UserSchema.methods.ispasswordcorrect = async function (password) {
     await bcrypt.compare(password,this.password)
 }
-UserSchema
+UserSchema.me
 export const User = mongoose.model("User",UserSchema)
