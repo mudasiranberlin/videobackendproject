898ca1fb9011fa8c55d199d0b0bc373cf3a994d1
Mudasir <apple.mudasir@gmail.com>
2026-07-14 14:43:55 +0700

    Auto commit

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -61,7 +61,7 @@ const registerUser = asyncHandler( async (req,res) => {
         username:username.toLowerCase()
     })
 
-    await User.findByid(user._id)
+    const await User.findByid(user._id)
 
 
     
