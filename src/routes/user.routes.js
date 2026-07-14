e656c738ae4a1315b72f192b9506fe3c2daea33a
Mudasir <apple.mudasir@gmail.com>
2026-07-14 13:46:37 +0700

    Auto commit

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,7 +5,7 @@ import {upload} from "../middlewares/multer.middleware.js"
 const router = Router()
 
 router.route('/register').post(
-    upload
+    upload.fields
 )
 
 router.route("/register").post(
