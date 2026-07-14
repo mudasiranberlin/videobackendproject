22c2a8383ae2900d52fb88b0d632fb21868de7b5
Mudasir <apple.mudasir@gmail.com>
2026-07-14 13:56:36 +0700

    Auto commit

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -4,6 +4,6 @@ const registerUser = asyncHandler( async (req,res) => {
     // get user details from frontend
     // validation not empty 
     // check if the user is already exist or not username and email
-    // 
+    // check 
 } )
 export {registerUser}
