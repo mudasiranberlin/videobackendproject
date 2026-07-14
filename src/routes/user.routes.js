99814a46c55af8816927703bf400a25a33a216b1
Mudasir <apple.mudasir@gmail.com>
2026-07-14 13:46:19 +0700

    Auto commit

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,18 +6,7 @@ const router = Router()
 
 router.route('/register').post()
 
-router.route("/register").post(
-    upload.fields([
-        {
-            name:"avatar",
-            maxCount:1
-        },
-        {
-            name:"coverImage",
-            maxCount:1
-        }
-    ]),
-    registerUser)
+
 
 export default router
 
