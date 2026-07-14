4d2caae83bd5983172dadc044c9ebf799f9a3689
Mudasir <apple.mudasir@gmail.com>
2026-07-14 14:46:52 +0700

    Auto commit

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -2,7 +2,7 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 import { ApiError } from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import { uploadCloudinary } from "../utils/cloudinary.js"
-ApiResponse
+
 
 const registerUser = asyncHandler( async (req,res) => {
     // get user details from frontend
