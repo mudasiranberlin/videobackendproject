33e6dfc3660f4d1de571c37a05bb708c9f5624fb
Mudasir <apple.mudasir@gmail.com>
2026-07-14 13:54:08 +0700

    Auto commit

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,8 +1,7 @@
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 const registerUser = asyncHandler( async (req,res) => {
-    res.status(200).json({
-        messsage:"ok"
+    
     })
 } )
 export {registerUser}
