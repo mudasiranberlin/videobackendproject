463affacb854b6eec4894c10b64d5252f6decd76
Mudasir <apple.mudasir@gmail.com>
2026-07-14 16:01:35 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,235 +1,79 @@
-// Import asyncHandler to handle errors automatically in async functions
-import { asyncHandler } from "../utils/asyncHandler.js"
-
-// Import custom error class to create our own errors
+import {asyncHandler} from "../utils/asyncHandler.js"
 import { ApiError } from "../utils/ApiError.js"
-
-// Import User model to interact with MongoDB User collection
-import { User } from "../models/user.models.js"
-
-// Import Cloudinary function to upload images
+import { ApiResponse } from "../utils/ApiResponse.js"
 import { uploadCloudinary } from "../utils/cloudinary.js"
 
-// Import custom API response format
-import { ApiResponse } from "../utils/ApiResponse.js"
+import {ApiResponse} from "../utils/cloudinary.js"
+
+const registerUser = asyncHandler( async (req,res) => {
+    // get user details from frontend
+    // validation not empty 
+    // check if the user is already exist or not username and email
+    // check for images check avatar
+    // upload from cloudnary 
+    // create user object entry in db 
+    // remove password and refresh token from response 
+    // check the user creation 
+    // return yes 
 
 
-// Register User function
-// asyncHandler will catch any error automatically
-const registerUser = asyncHandler(async (req, res) => {
+    const  {fullname,email,username,password} =req.body
+    console.log("email",email);
 
-
-    // Getting user information from frontend request body
-    // Example:
-    // req.body = {
-    // fullname:"Mudasir",
-    // username:"mudasir123",
-    // email:"test@gmail.com",
-    // password:"12345"
-    // }
-
-    const { fullname, username, email, password } = req.body
-
-
-    console.log(email, fullname)
-
-
-
-    // Checking if any required field is empty
-    // .some() checks if at least one value is empty
-    // .trim() removes extra spaces
+    if (fullname === "") {
+        throw new ApiError(400,"Full name is required")
+    }
 
     if (
-        [fullname, email, username, password].some(
-            (field) => field?.trim() === ""
-        )
+        [fullname,email,username,password].some((field)=>field?.trim() ==="" )
     ) {
 
-        // Stop execution and send error message
-        throw new ApiError(
-            400,
-            "All fields are required"
-        )
+        throw new ApiError(400,"All fields are required ")
     }
 
-
-
-    // Checking if user already exists in database
-    // Search by email OR username
-
-    const existedUser = await User.findOne({
-        $or: [
-            { email },
-            { username }
-        ]
+    const existedUser= User.findOne({
+        $or:[{username},{email}]
     })
-
-
-
-    // If user already exists
-    // Do not create duplicate account
 
     if (existedUser) {
-
-        throw new ApiError(
-            409,
-            "User with email or username already existed"
-        )
+        throw new ApiError(400,"Username  email already created ")
     }
 
-
-
-    // Getting avatar image path from uploaded files
-
-    // Example:
-    // req.files.avatar[0].path
-    // = "/uploads/profile.png"
-
-    const avatarlocalpath =
-        req.files?.avatar[0]?.path
-
-
-
-    // Getting cover image path
-
-    const coverimagelocalpath =
-        req.files?.coverImage[0]?.path
-
-
-
-
-    // Avatar image is required
-    // If user did not upload profile picture
+    const avatarlocalpath = req.files?.avatar[0]?.path;
+    const coverimagelocalpath = req.files?.avatar[0]?.path;
 
     if (!avatarlocalpath) {
-
-        throw new ApiError(
-            400,
-            "Avatar file is required"
-        )
+        throw new ApiError(409,"Everthing is Error ")
     }
 
-
-
-
-    // Upload avatar image to Cloudinary
-    // Local computer image
-    //       |
-    //       ↓
-    // Cloudinary storage
-    //       |
-    //       ↓
-    // Returns image URL
-
-    const avatar =
-        await uploadCloudinary(avatarlocalpath)
-
-
-
-    // Upload cover image to Cloudinary
-
-    const coverImage =
-        await uploadCloudinary(coverimagelocalpath)
-
-
-
-
-    // If avatar upload failed
+    const avatar= await uploadCloudinary(avatarlocalpath) 
+    const coverImage= await uploadCloudinary(coverimagelocalpath)
 
     if (!avatar) {
-
-        throw new ApiError(
-            400,
-            "Avatar file is required"
-        )
+        throw new ApiError(400,"Error")
+        
     }
-
-
-
-
-    // Create new user in MongoDB database
 
     const user = await User.create({
-
-        // User full name
         fullname,
-
-
-        // Save Cloudinary avatar URL
-        avatar: avatar.url,
-
-
-        // Save cover image URL
-        // If no cover image exists, save empty string
-
-        coverImage: coverImage?.url || "",
-
-
-        // User email
+        avatar:avatar.url,
+        coverImage:coverImage?.url || "",
         email,
-
-
-        // User password
         password,
-
-
-        // Convert username into lowercase
-        // Example:
-        // MUDASIR123 → mudasir123
-
-        username: username.toLowerCase()
-
+        username:username.toLowerCase()
     })
 
-
-
-
-    // Find the created user from database
-    // Remove password and refresh token
-    // because we should never send them to frontend
-
-    const createdUser =
-        await User.findById(user._id)
-        .select("-password -refreshToken")
-
-
-
-
-    // If user was not created successfully
+    const createdUser = await User.findByid(user._id).select("-passwors -refreshtoken")
 
     if (!createdUser) {
-
-        throw new ApiError(
-            500,
-            "Something went wrong while registering the user"
-        )
-
+        throw new ApiError(500,"Error creating the user")
     }
 
 
+return res.status(201).json(
+    new ApiResponse(200, createdUser,"User registred sucessfully")
+)
 
-
-    // Send success response back to frontend
-
-    return res.status(201).json(
-
-        new ApiResponse(
-
-            200,
-
-            createdUser,
-
-            "User registered successfully"
-
-        )
-
-    )
-
-})
-
-
-
-// Export this function
-// So we can use it in routes
-
-export default registerUser
+    
+} )
+export {registerUser}
