110e9657fc5cab3c8ddfb532e7568bc0370c4af7
Mudasir <apple.mudasir@gmail.com>
2026-07-14 13:56:22 +0700

    Auto commit

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -3,6 +3,7 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const registerUser = asyncHandler( async (req,res) => {
     // get user details from frontend
     // validation not empty 
-    // check if the user is already exist or not 
+    // check if the user is already exist or not username and email
+        
 } )
 export {registerUser}
