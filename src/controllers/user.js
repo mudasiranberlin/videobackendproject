bcb3d1f2a6daee669ed2b9db0628c0ca66cf1863
Mudasir <apple.mudasir@gmail.com>
2026-07-14 14:34:23 +0700

    Auto commit

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -44,7 +44,7 @@ const registerUser = asyncHandler( async (req,res) => {
         throw new ApiError(409,"Everthing is Error ")
     }
 
-     await uploadCloudinary(avatarlocalpath) 
+    const ava await uploadCloudinary(avatarlocalpath) 
 
 
     
