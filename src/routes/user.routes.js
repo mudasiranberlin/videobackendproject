3d5bef4bde6e1dd33926b93ae1e8d0c7e608052c
Mudasir <apple.mudasir@gmail.com>
2026-07-14 13:50:50 +0700

    Auto commit

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -18,11 +18,3 @@ router.route("/register").post(
     registerUser)
 
 export default router
-
-
-import { Router } from "express";
-import registerUser from "../controllers/user.controller.js";
-
-const router = Router()
-router.route("/register").post
-
