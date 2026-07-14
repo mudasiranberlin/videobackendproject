301df13c8af0623cd8f6b6124bec64aff035c01c
Mudasir <apple.mudasir@gmail.com>
2026-07-14 14:05:33 +0700

    Auto commit

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -25,7 +25,7 @@ const registerUser = asyncHandler( async (req,res) => {
         [fullname,email,username,password].some((field)=>field?.trim() ==="" )
     ) {
 
-        throw new ApiError(400,"All fields a")
+        throw new ApiError(400,"All fields are required ")
     }
 
     
