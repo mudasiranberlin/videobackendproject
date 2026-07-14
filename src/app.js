4828c26814ce4720b520d3b5a4711fc495fdb35b
Mudasir <apple.mudasir@gmail.com>
2026-07-14 11:40:54 +0700

    Auto commit

--- src/app.js
+++ src/app.js
@@ -58,7 +58,7 @@ app.use("/api/v1/users",userRouter);
 
 import userRouter from "./routes/user.routes.js"
 
-app.use("/api/v1/users",use)
+app.use("/api/v1/users",userRouter)
 
 
 export default app
