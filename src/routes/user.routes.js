f6192fe8780a2b72d4d17c5abfeffa9a73ad34fb
Mudasir <apple.mudasir@gmail.com>
2026-07-14 11:08:52 +0700

    remove unnecessary whitespace in user.routes.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -22,7 +22,7 @@ export default router
 
 import { Router } from "express";
 import registerUser from "../controllers/user.controller.js";
-
+//
 const router = Router()
 
 router.route("/register")
