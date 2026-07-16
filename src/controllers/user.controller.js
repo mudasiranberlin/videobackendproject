387d1cca2135c18498096249b8d0bfa45aef7d08
Mudasir <apple.mudasir@gmail.com>
2026-07-16 11:33:40 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -13,7 +13,7 @@ import { uploadCloudinary } from "../utils/cloudinary.js"
 // Import custom API response format
 import { ApiResponse } from "../utils/ApiResponse.js"
 
-const generateAccessAndRefreshTokens = async (user)
+const generateAccessAndRefreshTokens = async (userID)
 
 
 // Register User function
