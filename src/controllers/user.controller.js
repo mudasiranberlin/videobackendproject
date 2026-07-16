802bb37e0318263a65c014184ed3739bf3dd6f2d
Mudasir <apple.mudasir@gmail.com>
2026-07-16 11:33:10 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -13,7 +13,7 @@ import { uploadCloudinary } from "../utils/cloudinary.js"
 // Import custom API response format
 import { ApiResponse } from "../utils/ApiResponse.js"
 
-const generateAccess
+const generateAccessAnd
 
 
 // Register User function
