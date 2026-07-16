a20819c5cd018edf3738d48faedd1f64658c167d
Mudasir <apple.mudasir@gmail.com>
2026-07-16 11:45:24 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -274,7 +274,7 @@ const loginUser = asyncHandler( async (req,res)=>{
     }
 
    const {accessToken,refreshToken} = await generateAccessAndRefreshTokens(user._id)
-   const loggedInUser = await User.findById(user.)
+   const loggedInUser = await User.findById(user._id)
 
 
 
