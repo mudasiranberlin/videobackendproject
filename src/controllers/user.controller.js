c17b63a5c3d6bafcda4d96a0f77a66b00f06399b
Mudasir <apple.mudasir@gmail.com>
2026-07-16 11:20:57 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -229,7 +229,7 @@ console.log("req.files:", req.files);
 const loginUser = asyncHandler( async (req,res)=>{
     // req body ->data
     //check the username or password 
-    // 
+    // find the user
 
     
 })
