73712f5ed47ce4ab162b040de610d6e7d7442f9b
Mudasir <apple.mudasir@gmail.com>
2026-07-16 17:06:07 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -111,7 +111,7 @@ console.log("req.body:", req.body);
 
         let coverimagelocalpath;
 
-        if (req.files && Array.isArray(req.files.coverImage) && req.files) {
+        if (req.files && Array.isArray(req.files.coverImage) && req.files.co) {
             
         }
 
