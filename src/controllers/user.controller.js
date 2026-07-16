8dfdc817a2c6ca5f49182a763e98de8522620057
Mudasir <apple.mudasir@gmail.com>
2026-07-16 11:46:07 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -276,7 +276,9 @@ const loginUser = asyncHandler( async (req,res)=>{
    const {accessToken,refreshToken} = await generateAccessAndRefreshTokens(user._id)
    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
 
-   const options
+   const options = {
+    
+   }
 
 
 
