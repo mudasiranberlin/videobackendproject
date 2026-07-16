c8f21bdf0b7d5c6a2a1bed65ad2646f4a9274042
Mudasir <apple.mudasir@gmail.com>
2026-07-16 11:27:16 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -241,7 +241,7 @@ const loginUser = asyncHandler( async (req,res)=>{
         throw new ApiError("400","Username or email is required")
     }
 
-    User.findOne({
+    const user= User.findOne({
         $or:[{username},{email}]
     })
 
