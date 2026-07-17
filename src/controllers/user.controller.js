b5293a74c818311325e7091d3fab89e6e271a0e7
Mudasir <apple.mudasir@gmail.com>
2026-07-17 17:48:52 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -276,7 +276,9 @@ const loginUser = asyncHandler( async (req,res)=>{
 
     const isPasswordValid = await user.isPasswordCorrect(password)
 
-    if
+    if () {
+        
+    }
 
     const isPasswordValid = await user.isPasswordCorrect(password)
 
