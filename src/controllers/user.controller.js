795075d94aad25b371f42ccf5063ba6930744295
Mudasir <apple.mudasir@gmail.com>
2026-07-17 17:39:48 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -262,7 +262,7 @@ const loginUser = asyncHandler( async (req,res)=>{
     const {email,username,password} =req.body
 
     if (!username && !email) {
-        throw ApiError(400,)
+        throw ApiError(400,"Username a")
         
     }
 
