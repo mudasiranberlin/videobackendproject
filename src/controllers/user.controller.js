4ffcef59d3e3c22c23cc2c814f3ff2f6d9580461
Mudasir <apple.mudasir@gmail.com>
2026-07-18 17:19:20 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -301,7 +301,7 @@ const loginUser = asyncHandler( async (req,res)=>{
         throw new ApiError(401,"Invalid user credentials") 
     }
 
-    const 
+    const {accessToken}
 
    const {accessToken,refreshToken} = await generateAccessAndRefreshTokens(user._id)
    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
