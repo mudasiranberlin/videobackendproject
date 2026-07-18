c06da8ea7ea9a69140e5b43240d0e3a013648026
Mudasir <apple.mudasir@gmail.com>
2026-07-18 17:11:50 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -269,7 +269,7 @@ console.log("req.files:", req.files);
 const generateAccessAndRefreshTokens = async (userId) => {
     try {
         const user = await findById(userId)
-        const generateRefreshToken = user.generateRefreshToken
+        const generateReaccreshToken = user.generateRefreshToken
         const generateAccessToken = user.generateAccessToken
         
     } catch (error) {
