4d86ee3d8f421046ef1ac99fcb99a0e5656d91ad
Mudasir <apple.mudasir@gmail.com>
2026-07-18 16:55:22 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -279,7 +279,7 @@ const loggedInUser = asyncHandler (async (req,res) => {
     if (!user) {
         throw new ApiError(401,"User does not exist")
     }
-    const isPasswordValid = await isp
+    const isPasswordValid = await isPasswordCorrect 
 
     
 
