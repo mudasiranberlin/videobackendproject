f0e213fa75f8fd3c81121b2f81e0287ae126b865
Mudasir <apple.mudasir@gmail.com>
2026-07-19 17:38:31 +0700

    Auto commit

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -12,4 +12,8 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
     const user = await User.findById(decodedToken?._id).select("-password, -refreshToken")
 
+    if (condition) {
+        
+    }
+
  })
