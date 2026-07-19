5266745dec9b5ff889be2c548827984f52ca71f0
Mudasir <apple.mudasir@gmail.com>
2026-07-19 17:12:19 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -15,7 +15,7 @@ import { ApiResponse } from "../utils/ApiResponse.js"
 
 
 
-const generateAccessAndRefreshTokens=
+const generateAccessAndRefreshTokens = as
 
 const generateAccessAndRefreshTokens = async (userId)=>{
     try {
