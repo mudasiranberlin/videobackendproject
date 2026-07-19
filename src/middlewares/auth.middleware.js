352b1665b23f6597061d415459fda2f49b45f2be
Mudasir <apple.mudasir@gmail.com>
2026-07-19 18:08:22 +0700

    Auto commit

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -6,7 +6,7 @@ import { User } from "../models/user.models.js"
  export const verfiyJWT = asyncHandler(async (req,res,next)=>{
     try {
         const token = req.cookies?.accessToken || req.header
-        ("Authorization")?.replace("Bearer","")
+        ("Authorization")?.replace("Bearer ","")
     
         if (!token) {
             throw new ApiError(401,"Cannot find the resh token")
