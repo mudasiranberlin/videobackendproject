c2afe7c9ea95cbf5ebbb19d2a67b77582ac3d3b1
Mudasir <apple.mudasir@gmail.com>
2026-07-19 17:35:26 +0700

    Auto commit

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -10,17 +10,3 @@ import {asyncHandler} from "../utils/asyncHandler.js"
     }
 
  })
-
-
- import { ApiError } from "../utils/ApiError.js"
- import { asyncHandler } from "../utils/asyncHandler.js"
-
- export const verfiyJWT = asyncHandler( async(req,res,next)=>{
-    const token = req.cookies?.accessToken || req.header
-    ("Authorization")?.replace("Bearer","")
-    if (!token) {
-        throw new ApiError(401,"cannot find the refrsh token")
-        
-    }
-
- } )
