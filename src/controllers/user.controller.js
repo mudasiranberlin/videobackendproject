fe1e88bb27f4138e7e802d4385ef8e38bca17d0b
Mudasir <apple.mudasir@gmail.com>
2026-07-19 17:24:02 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -295,7 +295,7 @@ const loginUser = asyncHandler( async (req,res)=>{
    return res.status(200)
    .cookie("accessToken",accessToken,options)
    .cookie("refreshToken",refreshToken,options)
-   
+   .json
 
    const options = {
     httpOnly : true,
