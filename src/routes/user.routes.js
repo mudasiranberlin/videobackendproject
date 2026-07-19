1401b02fe577cdd1dd8365d68cfc6fbfdc2b9177
Mudasir <apple.mudasir@gmail.com>
2026-07-19 17:42:36 +0700

    Auto commit

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {registerUser} from "../controllers/user.controller.js";
+import {loginUser, registerUser} from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 
 const router = Router()
@@ -17,6 +17,7 @@ router.route("/register").post(
     ]),
     registerUser)
 
-    router.route("login").post(log)
+    router.route("login").post(loginUser)
+    router
 
 export default router
