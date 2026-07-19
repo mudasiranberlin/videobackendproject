71baee35d65a958c031e139953217509cb507024
Mudasir <apple.mudasir@gmail.com>
2026-07-19 17:13:34 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -17,7 +17,7 @@ import { ApiResponse } from "../utils/ApiResponse.js"
 
 const generateAccessAndRefreshTokens = async (userId)=>{
     try {
-        const user = await user
+        const user = await Userfind
         
     } catch (error) {
         throw new ApiError(500,"SOmething went wrong generating refresh token")
