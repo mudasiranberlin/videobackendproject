f59905fe13262fd2061569637ad459cf59f5ff3e
Mudasir <apple.mudasir@gmail.com>
2026-07-19 17:37:00 +0700

    Auto commit

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -8,6 +8,6 @@ import {asyncHandler} from "../utils/asyncHandler.js"
     if (!token) {
         throw new ApiError(401,"Cannot find the resh token")
     }
-    jwt.verify(token,process.env.ASSESS_TOKEN_SECRET)
+    const jwt.verify(token,process.env.ASSESS_TOKEN_SECRET)
 
  })
