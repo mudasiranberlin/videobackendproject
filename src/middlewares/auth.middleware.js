5deae8fb14598227a7533c49afe17ff8ddf3db41
Mudasir <apple.mudasir@gmail.com>
2026-07-19 17:39:58 +0700

    Auto commit

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -2,6 +2,25 @@ import { ApiError } from "../utils/ApiError.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
  export const verfiyJWT = asyncHandler(async (req,res,next)=>{
-    try
+    try {
+        const token = req.cookies?.accessToken || req.header
+        ("Authorization")?.replace("Bearer","")
+    
+        if (!token) {
+            throw new ApiError(401,"Cannot find the resh token")
+        }
+        const decodedToken = jwt.verify(token,process.env.ASSESS_TOKEN_SECRET)
+    
+        const user = await User.findById(decodedToken?._id).select("-password, -refreshToken")
+    
+        if (!user) {
+    
+            throw new ApiError(401,"Invalid access token")
+            req.user =user
+            next()
+        }
+    } catch (error) {
+        
+    }
 
  })
