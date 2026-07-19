8103c71a5e5b9bcc511a672df49f16a54192d502
Mudasir <apple.mudasir@gmail.com>
2026-07-19 17:46:36 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -308,7 +308,9 @@ const loginUser = asyncHandler( async (req,res)=>{
 const logoutUser = asyncHandler(async (req,res) => {
     User.findByIdAndUpdate(
         req.user._id,{
-            $set:{}
+            $set:{
+                
+            }
         }
     )
     req.user._id
