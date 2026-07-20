e45e7462cce89fed0db81cdb33b8c1a054bb101f
Mudasir <apple.mudasir@gmail.com>
2026-07-20 17:26:03 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -27,7 +27,7 @@ const generateAccessAndRefereshTokens = async (userId)=>{
 
     user.refreshToken = refreshToken
     await user.save({validateBeforeSave:true})
-    return (refreshToken,accessToken)options
+    return (refreshToken,accessToken)
         
     } catch (error) {
         throw new ApiError(401,"Unable to gnerate the refresh token ")
