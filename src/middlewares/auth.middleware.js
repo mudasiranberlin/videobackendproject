1e6af33415c94573d079dc252e9eaad6c92f7a2b
Mudasir <apple.mudasir@gmail.com>
2026-07-20 17:47:31 +0700

    Auto commit

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -35,5 +35,5 @@ import { User } from "../models/user.models.js"
     if (!token) {
         throw new ApiError(401,"Unauthorized request");
     }
-    jwt.verify(token.process.env.A)
+    jwt.verify(token.process.env.ACCESS_TOKEN_SECRET)
  } )
