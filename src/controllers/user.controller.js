b4303ac827c172a7309d2e8c57cec1e895965368
Mudasir <apple.mudasir@gmail.com>
2026-07-20 17:16:48 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -21,7 +21,7 @@ import mongoose from "mongoose";
 
 const generateAccessAndRefereshTokens = async (User)=>{
     try {
-        const user = find
+        const user = findById()
     const refreshToken = user.generateRefreshToken()
     const accessToken = user.generateAccessToken()
 
