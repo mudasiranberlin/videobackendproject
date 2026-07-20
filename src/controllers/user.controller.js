b3ce3abaa30a353d1d07a16e51216ef0018cb597
Mudasir <apple.mudasir@gmail.com>
2026-07-20 17:28:34 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -318,7 +318,7 @@ const loggedInUser = asyncHandler( async (req,res)=>{
     }
     return res.status(200)
     .cookie("accesstoken",accessToken,options)
-    .cookie("ref")
+    .cookie(refreshToken)
 
     
 }) 
