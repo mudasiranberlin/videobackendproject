741b7d2ad8dbe54cea6babbff4f1fb27f5c3c200
Mudasir <apple.mudasir@gmail.com>
2026-07-21 17:32:38 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -362,7 +362,7 @@ const refreshAccessToken = asyncHandler( async(req,res)=>{
 
         
     }
-    const decodedToken= jwt.verify(
+    const decodedToken = jwt.verify(
         incomingRefreshToken,
         process.env.REFRESH_TOKEN_SECRET
     )
