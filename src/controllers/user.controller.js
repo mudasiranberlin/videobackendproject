6c7572f5d2b1101967bb4e42f64c837dfd34f46e
Mudasir <apple.mudasir@gmail.com>
2026-07-21 17:26:53 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -355,8 +355,8 @@ const logoutUser = asyncHandler(async (req,res) => {
 // So we can use it in routes
 // 
 
-const refreshAccessToken = asyncHandler( async()=>{
-    
+const refreshAccessToken = asyncHandler( async(req,res)=>{
+
 } )
 
 export  {
