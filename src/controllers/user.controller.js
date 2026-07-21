88dd79805d73e48afddaafc33e38505ad1bdc71c
Mudasir <apple.mudasir@gmail.com>
2026-07-21 17:34:30 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -367,6 +367,7 @@ const refreshAccessToken = asyncHandler( async(req,res)=>{
         process.env.REFRESH_TOKEN_SECRET
     )
     const user = User.findById(decodedToken?._id)
+    
     if (!user) {
         throw new ApiError(401,"Invalid refresh token")
     }
