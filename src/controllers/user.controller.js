7619c37e0fe174e5de14ca355e642251ccde1ec4
Mudasir <apple.mudasir@gmail.com>
2026-07-21 17:42:10 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -379,7 +379,7 @@ const refreshAccessToken = asyncHandler( async(req,res)=>{
         httpOnly: true,
         secure:true
     }
-    const await generateAccessAndRefereshTokens(user._id)
+    const {accessToken} await generateAccessAndRefereshTokens(user._id)
     return res
     .status(200)
     .cookie("accessToken",accessToken,options)
