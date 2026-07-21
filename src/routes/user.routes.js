5acfebf84eb061957cdf48a3c745187bb4dfe90d
Mudasir <apple.mudasir@gmail.com>
2026-07-21 17:47:44 +0700

    Auto commit

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {loginUser, logoutUser, registerUser} from "../controllers/user.controller.js";
+import {loginUser, logoutUser, registerUser,refreshAccessToken} from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verfiyJWT } from "../middlewares/auth.middleware.js";
 
