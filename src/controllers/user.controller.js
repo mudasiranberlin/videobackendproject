10c876f7c0a2444477f17462bab19c150359a299
Mudasir <apple.mudasir@gmail.com>
2026-07-22 18:35:14 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -415,7 +415,7 @@ const refreshAccessToken = asyncHandler( async (req,res)=>{
         httpOnly:true,
         secure:true
     }
-    generateAccessAndRefereshTokens(user)
+    generateAccessAndRefereshTokens(user_)
 })
 
 export  {
