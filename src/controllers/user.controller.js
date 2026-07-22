74de49f8e61b0f73e95a6a62c073569d00c59e70
Mudasir <apple.mudasir@gmail.com>
2026-07-22 18:26:03 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -398,7 +398,7 @@ const refreshAccessToken = asyncHandler( async(req,res)=>{
 const refreshAccessToken = asyncHandler( async (req,res)=>{
     const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
     if (!incomingRefreshToken) {
-        throw new ApiError
+        throw new ApiError("")
         
     }
 })
