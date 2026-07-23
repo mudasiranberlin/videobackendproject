b6251a7f3be5670ca4fb1fb84766d1bcf8d612ac
Mudasir <apple.mudasir@gmail.com>
2026-07-23 17:37:47 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -409,7 +409,9 @@ const changeCurrentPassword = asyncHandler(async (req,res)=>{
     return res.status(200).json(new ApiResponse(200,{},"password changed sucessfully"))
 })
 
-const getCurrentUser = asyncHandler(async(req,res)=>)
+const getCurrentUser = asyncHandler(async(req,res)=>{
+    
+})
 export  {
     registerUser,
     loginUser,
