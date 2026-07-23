14615fa0162c7be44363e329b97a3706f947a1e0
Mudasir <apple.mudasir@gmail.com>
2026-07-23 17:35:06 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -406,7 +406,7 @@ const changeCurrentPassword = asyncHandler(async (req,res)=>{
     user.password = new password
     await user.save({validateBeforeSave:false})
 
-    retur
+    return res.status
 })
 
 export  {
