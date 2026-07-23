b0cd53938e2ca87a269ca5cb8e9f718dc2940ec4
Mudasir <apple.mudasir@gmail.com>
2026-07-23 17:38:09 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -411,7 +411,7 @@ const changeCurrentPassword = asyncHandler(async (req,res)=>{
 
 const getCurrentUser = asyncHandler(async(req,res)=>{
     return res
-    .status
+    .status(200)
 })
 export  {
     registerUser,
