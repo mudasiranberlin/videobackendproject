837070b07deed65876549b3741123b159fee993f
Mudasir <apple.mudasir@gmail.com>
2026-07-23 16:59:02 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -432,7 +432,7 @@ const refreshAccessToken = asyncHandler(async(req,res)=>{
         throw new ApiError(401,"Unatorized access")
     }
     jwt.verify(
-        incomingRefreshToken = ref
+        incomingRefreshToken = pr
     )
 })
 
