73e8db2dca952e6400e1b40e619fe7b1c2ecc4e4
Mudasir <apple.mudasir@gmail.com>
2026-07-23 17:09:38 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -425,7 +425,7 @@ const refreshAccessToken = asyncHandler( async (req,res)=>{
             
         })
     } catch (error) {
-        throw new ApiResponse()
+        throw new ApiResponse(401,)
         
     }
 })
