a69f5d9979db9eb075fb4eef48c45e7ffb1c4d24
Mudasir <apple.mudasir@gmail.com>
2026-07-23 17:02:24 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -438,7 +438,7 @@ const refreshAccessToken = asyncHandler(async(req,res)=>{
     const user = await User.findById(decodedToken?._id)
 
     if (!user) {
-        throw new ApiError()
+        throw new ApiError(201,"")
         
     }
 })
