85e53fce1b47ed1d73954ec69258863bdf4b71a3
Mudasir <apple.mudasir@gmail.com>
2026-07-23 17:30:11 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -398,7 +398,7 @@ const refreshAccessToken = asyncHandler( async(req,res)=>{
 
 const changeCurrentPassword = asyncHandler(async (req,res)=>{
     const {oldPassword,newPassword} = req.body
-    const user = User.findById(req)
+    const user = User.findById(req.user)
 })
 
 export  {
