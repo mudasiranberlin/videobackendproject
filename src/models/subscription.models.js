b59247be55b16d72cb09afa92e05e6cb10345539
Mudasir <apple.mudasir@gmail.com>
2026-07-23 17:22:22 +0700

    Auto commit

--- src/models/subscription.models.js
+++ src/models/subscription.models.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const SubscriptionScheme = new mongoose.Schema({
 
     subscriber:{
-        type:str
+        type:String,
+        
     }
 
 },{timestamps:true})
