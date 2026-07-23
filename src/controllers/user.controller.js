9721a6f1692bab1cda3a5fb365442ef64691b4bf
Mudasir <apple.mudasir@gmail.com>
2026-07-23 17:03:32 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -445,7 +445,8 @@ const refreshAccessToken = asyncHandler(async(req,res)=>{
         throw new ApiError(201,"Invalid refesh token")
     }
     const options={
-        httpOnly
+        httpOnly:true
+        
     }
 })
 
