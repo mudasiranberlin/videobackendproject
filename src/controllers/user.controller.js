3c51a2e34ea11e759a62dca893957cb659f00c5b
Mudasir <apple.mudasir@gmail.com>
2026-07-23 17:32:56 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -401,7 +401,7 @@ const changeCurrentPassword = asyncHandler(async (req,res)=>{
     const user =  await User.findById(req.user?.id)
     const isPasswordCorrect= await user.isPasswordCorrect(oldPassword)
     if (!isPasswordCorrect) {
-        throw new ApiError(401,"Plese enter correct Password")
+        throw new ApiError(401,"Please enter correct Password")
     }
 })
 
