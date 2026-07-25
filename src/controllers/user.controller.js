f588501ae4e70fdcaa4cf28814e83ed7c7d60a35
Mudasir <apple.mudasir@gmail.com>
2026-07-25 17:05:30 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -466,7 +466,7 @@ const updateUserAvatar = asyncHandler( async (req,res)=>{
 
     const user =  await User.findByIdAndUpdate(req.user?._id,
         {
-            
+            $set
         }
     )
 
