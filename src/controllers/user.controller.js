c944905ac7fbea578387974173f37def80db9284
Mudasir <apple.mudasir@gmail.com>
2026-07-25 16:51:14 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -433,7 +433,7 @@ const updateAccountDetails = asyncHandler( async(req,res)=>{
         throw new ApiError(400,"All fields are required")
         
     }
-    await User.findByIdAndUpdate(req.user)
+    await User.findByIdAndUpdate(req.user>)
 
 } )
 
