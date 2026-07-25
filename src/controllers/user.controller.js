bac628c4784874485d89eab148cf38b86e4c5f50
Mudasir <apple.mudasir@gmail.com>
2026-07-25 17:04:36 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -460,7 +460,7 @@ const updateUserAvatar = asyncHandler( async (req,res)=>{
     }
 
     if (!avatar.url) {
-        throw new ApiError(400,"Error While ")
+        throw new ApiError(400,"Error While uploading")
         
     }
 
