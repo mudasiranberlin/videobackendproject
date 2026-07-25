076113cce66da80e9a75f2fc955898b13ed44fa8
Mudasir <apple.mudasir@gmail.com>
2026-07-25 17:11:19 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -474,7 +474,7 @@ const updateUserAvatar = asyncHandler( async (req,res)=>{
     ).select("-password")
 } ) 
 
-const updateUserCover = asyncHandler( async (req,res)=>{
+const updateUserCoverImage = asyncHandler( async (req,res)=>{
 
     const avatarLocalPath = req.file?.path
     if (avatarLocalPath) {
