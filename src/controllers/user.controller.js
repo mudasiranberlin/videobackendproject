9575fc12adae6c9e6d623c1430140cb0077724ff
Mudasir <apple.mudasir@gmail.com>
2026-07-25 17:11:04 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -474,7 +474,28 @@ const updateUserAvatar = asyncHandler( async (req,res)=>{
     ).select("-password")
 } ) 
 
+const updateUserAvatar = asyncHandler( async (req,res)=>{
 
+    const avatarLocalPath = req.file?.path
+    if (avatarLocalPath) {
+        throw new ApiError(400,"Avatar file is Missing")
+        
+    }
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+
+    if (!avatar.url) {
+        throw new ApiError(400,"Error While uploading on avatar")
+        
+    }
+
+    const user =  await User.findByIdAndUpdate(req.user?._id,
+        {
+            $set:
+            { avatar:avatar.url}
+        },
+        {new : true}
+    ).select("-password")
+} ) 
 
 
 
