b565a75b4e0b7f04064bd6de1e6892adf9d10957
Mudasir <apple.mudasir@gmail.com>
2026-07-27 17:21:44 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -511,7 +511,7 @@ const getUserChannelProfile = asyncHandler(async(res,req)=>{
     const {username}=req.params
     
     if (!username) {
-        throw new ApiError(401,"")
+        throw new ApiError(401,"User")
         
     }
 })
