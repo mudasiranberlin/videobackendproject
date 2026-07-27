a80bf95dd8f227d980dfc7bc471fc068ce86fdac
Mudasir <apple.mudasir@gmail.com>
2026-07-27 17:22:26 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -513,7 +513,7 @@ const getUserChannelProfile = asyncHandler(async(res,req)=>{
     if (!username) {
         throw new ApiError(401,"User not fond ")
 
-        User.aggregate
+        User.aggregate([])
         User.findOne({username})
         
     }
