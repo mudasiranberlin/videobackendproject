5f1513127f41782bed8ace352155777cd473ef9f
Mudasir <apple.mudasir@gmail.com>
2026-07-27 17:21:02 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -508,7 +508,7 @@ const updateUserCoverImage = asyncHandler( async (req,res)=>{
 
 
 const getUserChannelProfile = asyncHandler(async(res,req)=>{
-    const {username}=req 
+    const {username}=req. 
 })
 
 
