19437e19b3f77ae712ec84f655254ef2bec011a4
Mudasir <apple.mudasir@gmail.com>
2026-07-29 21:50:05 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -628,7 +628,7 @@ const loginUser = asyncHandler ( async(req,res)=>{
 
                 user.refreshToken = refreshToken;
                 user.save(
-                    {validateBeforeSave:}
+                    {validateBeforeSave:true}
                 )
                 
             } catch (error) {
