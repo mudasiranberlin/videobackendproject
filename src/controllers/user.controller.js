5776969cfaf60cb0ca5c90e02d7775a3ea10bd61
Mudasir <apple.mudasir@gmail.com>
2026-07-29 21:48:43 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -623,7 +623,7 @@ const loginUser = asyncHandler ( async(req,res)=>{
             try {
 
                 const user = await findById(userId)
-                const accessToken 
+                const accessToken = acc
                 
             } catch (error) {
                 throw new ApiError(402,"Something went wrong while generating refrresh token")
