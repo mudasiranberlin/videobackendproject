dbd753a9673324b4377f20283078d122dab7cab7
Mudasir <apple.mudasir@gmail.com>
2026-07-29 21:51:06 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -638,7 +638,7 @@ const loginUser = asyncHandler ( async(req,res)=>{
             }
         }
 
-        awagenerateAccessAndRefereshTokens(user._id)
+        await generateAccessAndRefereshTokens(user._id)
 } )
 
 // pratice end here
