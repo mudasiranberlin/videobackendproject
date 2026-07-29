8b183df17b5c1a4b389e0653298f1f8febaa29c5
Mudasir <apple.mudasir@gmail.com>
2026-07-29 14:46:08 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -577,7 +577,7 @@ const getUserChannelProfile = asyncHandler(async(req,res)=>{
             },
             {
                 $lookup:{
-                    
+                    from
                 }
             }
         ])
