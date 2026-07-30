3eac4f355964e59ff751f28874da891ddca7b8e6
Mudasir <apple.mudasir@gmail.com>
2026-07-30 17:48:07 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -670,7 +670,7 @@ const getWatchHistory = asyncHandler( async()=>{
     const user = await User.aggregate([
         {
             $match:{
-                _id:new mongoose.Types.ObjectId(req.user)
+                _id:new mongoose.Types.ObjectId(req.user._id)
             }
         }
     ])
