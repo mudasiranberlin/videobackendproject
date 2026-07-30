be682a67ed6aa924d2f4df57513d365a193010e5
Mudasir <apple.mudasir@gmail.com>
2026-07-30 17:28:07 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -544,7 +544,7 @@ const getUserChannelProfile = asyncHandler(async (req,res)=>{
                 subscribersCount:{
                     $size: "$subscribers"
                 },
-                
+                chan
             }
         }
     ])
