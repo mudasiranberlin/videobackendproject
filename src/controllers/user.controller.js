0e5ac6a54994421bb54e8a1bea31b6ffb199fe07
Mudasir <apple.mudasir@gmail.com>
2026-07-30 17:30:43 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -550,7 +550,7 @@ const getUserChannelProfile = asyncHandler(async (req,res)=>{
 
                 isSubscribed:{
                     $cond:{
-                        if:{$in:[req.user?.id_]}
+                        if:{$in:[req.user?.id,]}
                     }
                 }
 
