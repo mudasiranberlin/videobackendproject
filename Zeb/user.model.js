b67e2e50fb80d4898eb196c64dfe59404222a258
Mudasir <apple.mudasir@gmail.com>
2026-07-31 10:30:30 +0700

    Auto commit

--- Zeb/user.model.js
+++ Zeb/user.model.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const orderSchema = new mongoose.Schema({
     productid:{
-        type:mongoose.Schema.Types.ObjectId
+        type:mongoose.Schema.Types.ObjectId,
+        ref
     }
 })
 
