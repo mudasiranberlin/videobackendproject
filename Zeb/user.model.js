4a9ff40e73af6fbc1a7f8234dd939a6d9324caa8
Mudasir <apple.mudasir@gmail.com>
2026-07-31 10:31:05 +0700

    Auto commit

--- Zeb/user.model.js
+++ Zeb/user.model.js
@@ -5,7 +5,7 @@ const orderSchema = new mongoose.Schema({
         type:mongoose.Schema.Types.ObjectId,
         ref:"product"
     },
-    qunat
+    quntatity 
 })
 
 const UserSchema = new mongoose.Schema({
