88d263c5bea7b5b9ea15e786aaf5ba8a39a00130
Mudasir <apple.mudasir@gmail.com>
2026-08-01 13:56:28 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1187,7 +1187,7 @@ const updateAccountDetails = asyncHandler(async(req,res)=>{
             fullname:fullname,
             email:email
         }
-    },{new:true}).select("-password -refreshtoken")
+    },{new:true}).select("-password")
 
 })
 
