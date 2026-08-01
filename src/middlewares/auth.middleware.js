e3b5bdb084fc14d6e6f67b020f19060e23fb1848
Mudasir <apple.mudasir@gmail.com>
2026-08-01 09:37:50 +0700

    Auto commit

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -32,6 +32,6 @@ import { User } from "../models/user.models.js"
 
  import { asyncHandler } from "../utils/asyncHandler.js"
 
- export const verfiyJWT = asyncHandler( as )
+ export const verfiyJWT = asyncHandler( asy )
 
  
