de13ea1f3782f72ceefccb6fcfee16eda79c6bd7
Mudasir <apple.mudasir@gmail.com>
2026-08-01 09:55:27 +0700

    Auto commit

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -52,7 +52,6 @@ here we have next also we use middle ware next andwhen it will done and then pas
 
 
 // Why .replace("Bearer ", "")?
-
 // JWT only needs the token.
 // But the header contains
 // Bearer abc123
