44915dc27e7a97cb4356a3cb60d66bc4146661ce
Mudasir <apple.mudasir@gmail.com>
2026-08-01 10:04:49 +0700

    Auto commit

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -86,7 +86,7 @@ here we have next also we use middle ware next andwhen it will done and then pas
     req.user= user
     next()
     } catch (error) {
-        throw new
+        throw new ApiError()
         
     }
  } )
