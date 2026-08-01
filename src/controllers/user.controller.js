b7e0e3be72887d95ee758acb4ae56f7366d9c18b
Mudasir <apple.mudasir@gmail.com>
2026-08-01 11:38:11 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -858,7 +858,7 @@ const incomingRefreshToken = asyncHandler(async()=>{
         httpOnly=true,
         secure:true
     }
-    const {}await generateAccessAndRefereshTokens(user_id)
+    const {} = await generateAccessAndRefereshTokens(user_id)
 
 
 })
