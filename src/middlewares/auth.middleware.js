581b49d27e94c68fa8b3c6b918784e02f1b7b0c8
Mudasir <apple.mudasir@gmail.com>
2026-08-01 09:43:43 +0700

    Auto commit

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -35,7 +35,9 @@ import { User } from "../models/user.models.js"
 # name can be anything here i give the name is verifyjwt 
 
 
- export const verfiyJWT = asyncHandler( async(req,res,)=>{
+ export const verfiyJWT = asyncHandler( async(req,res,next
+    
+ )=>{
     req.cookies?.accessToken || req.header
  } )
 
