35a7a59e3141ae6f56976a804d93ae2ed1476f45
Mudasir <apple.mudasir@gmail.com>
2026-08-01 11:33:13 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -844,7 +844,7 @@ const incomingRefreshToken = asyncHandler(async()=>{
         throw new ApiError(401,"Unauthorized access")
         
     }
-    jwt.verify(incomingRefreshToken,process.env.refreshAccessToken)
+    jwt.verify(incomingRefreshToken,process.env.acc)
 })
 //End of the practice
 
