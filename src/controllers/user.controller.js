275c47f4a90c10b432ffbe72e07cc14a69f22957
Mudasir <apple.mudasir@gmail.com>
2026-08-01 09:13:27 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -777,8 +777,7 @@ const getWatchHistory = asyncHandler(async (req, res) => {
 const loginUser = asyncHandler(async(req,res)=>{
     const {username,password,email}= res.body
     if (!username && password) {
-        throw new ApiError(410,"Please enter the username and password");
-        
+        throw new ApiError(410,"Please enter the username and password");   
     }
 
 })
