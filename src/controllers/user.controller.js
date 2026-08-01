3fc0f3fa8487c066566616062b6c67382ec35195
Mudasir <apple.mudasir@gmail.com>
2026-08-01 09:31:31 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -814,7 +814,7 @@ const loginUser = asyncHandler(async(req,res)=>{
     const loggedInUser =await user.findById(user._id).select("-password-refreshToken")
 
     const options:{
-        
+        httpOnly
     }
     
 
