43b4c8c30d71e80b7864d1e583c10b2fcf6a9dab
Mudasir <apple.mudasir@gmail.com>
2026-08-01 13:52:05 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1179,7 +1179,7 @@ const getCurrentUser = asyncHandler( async(req,res)=>{
 
 const updateAccountDetails = asyncHandler(async(req,res)=>{
     const {username,fullname} = req.body
-    if (fullname,use) {
+    if (fullname,username) {
         
     }
 })
