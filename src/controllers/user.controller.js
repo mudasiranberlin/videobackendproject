3c5022354d17254d0bd4658673fc92e7add287f4
Mudasir <apple.mudasir@gmail.com>
2026-08-01 13:19:22 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -908,7 +908,7 @@ const incomingRefreshToken = asyncHandler(async()=>{
 //get user password 
 
 //  Step 2
-const user = await User.findById(req.user?._id);
+//  const user = await User.findById(req.user?._id);
 Where did req.user come from?
 
 From your verifyJWT middleware.
