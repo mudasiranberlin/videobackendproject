2c61117a6fd0596f879e95fe0c285d1a5db08871
Mudasir <apple.mudasir@gmail.com>
2026-08-01 13:14:27 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -886,8 +886,7 @@ const changeCurrentPassword = asyncHandler( async(req,res)=>{
 
     user.password = newPassword  //save password.  now it will go to pre and then it will get hash userSchema.pre()
     await user.save({
-        validateBeforeSave:false
-    })
+        validateBeforeSave:false})
 })
 
 
