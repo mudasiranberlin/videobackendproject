82fddbb73aa48ddf1acdaf556611c030167c72f6
Mudasir <apple.mudasir@gmail.com>
2026-08-01 13:55:18 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1178,7 +1178,7 @@ const getCurrentUser = asyncHandler( async(req,res)=>{
 
 
 const updateAccountDetails = asyncHandler(async(req,res)=>{
-    const {username,fullname} = req.body
+    const {email,fullname} = req.body
     if (!fullname ||!email) {
         throw new ApiError(201,"Enter the fullname and username")
     }
