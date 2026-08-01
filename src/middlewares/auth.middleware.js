7b11d3c3f79b412156a36f178e5d1b337f84979e
Mudasir <apple.mudasir@gmail.com>
2026-08-01 10:03:03 +0700

    Auto commit

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -78,7 +78,9 @@ const decodedToken =jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
 
 await User.findById(decodedToken?._id).select("-password -refreshtoken")
 
-if
+if (user) {
+    
+}
  } )
 
  
