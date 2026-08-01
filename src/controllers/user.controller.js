ef3abb3ab5e5a821adc5e3897ded0d70fb9c1558
Mudasir <apple.mudasir@gmail.com>
2026-08-01 09:22:44 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -792,7 +792,9 @@ const loginUser = asyncHandler(async(req,res)=>{
         throw new ApiError(401,"password incoorect");
     }
 
-    const generateAccessAndRefereshTokens = async()
+    const generateAccessAndRefereshTokens = async()=>{
+        
+    }
     
 
 
