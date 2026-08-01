1168b26cc9d9aad47be315be1cbb399258cfacbb
Mudasir <apple.mudasir@gmail.com>
2026-08-01 09:43:59 +0700

    Auto commit

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -33,7 +33,7 @@ import { User } from "../models/user.models.js"
  import { asyncHandler } from "../utils/asyncHandler.js"
 
 # name can be anything here i give the name is verifyjwt 
-here we have next 
+here we have next also we use middle ware and 
 
  export const verfiyJWT = asyncHandler( async(req,res,next
  )=>{
