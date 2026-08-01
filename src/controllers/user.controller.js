a9ad5bf9f1d4a721fb9b14802965c36aedf4abdd
Mudasir <apple.mudasir@gmail.com>
2026-08-01 11:36:20 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -854,6 +854,7 @@ const incomingRefreshToken = asyncHandler(async()=>{
     if (incomingRefreshToken !== user?.refreshToken ) {
         throw new ApiError(403,"refresh token expired")
     }
+    
 })
 //End of the practice
 
