01253478126688b981755aa4f684f5c6c0b3a2d0
Mudasir <apple.mudasir@gmail.com>
2026-08-01 11:35:51 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -851,7 +851,7 @@ const incomingRefreshToken = asyncHandler(async()=>{
         throw new ApiError(403,"Unauthorized access")
     }
 
-    if (incomingRefreshToken !== user?. ) {
+    if (incomingRefreshToken !== user?.refreshToken ) {
         
     }
 })
