48dbc20c3141239df23f418b055a1c281de90164
Mudasir <apple.mudasir@gmail.com>
2026-08-01 13:21:18 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -948,8 +948,7 @@ const incomingRefreshToken = asyncHandler(async()=>{
 
  // const isPasswordCorrect =
      // await user.isPasswordCorrect(oldPassword);
-
-     What is isPasswordCorrect()?
+What is isPasswordCorrect()?
 
 In your User model, you probably have:
 
