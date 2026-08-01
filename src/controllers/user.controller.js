308951e6b5d0fe515b40ae3e9fc183a0fa9bd145
Mudasir <apple.mudasir@gmail.com>
2026-08-01 09:33:47 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -823,7 +823,7 @@ const loginUser = asyncHandler(async(req,res)=>{
     .json(
         new ApiResponse(200,
             {
-                user:loo
+                user:loog
             }
         )
 
