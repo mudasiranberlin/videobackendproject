479932a763a5b20da91d2c317411ff4720ae4cd4
Mudasir <apple.mudasir@gmail.com>
2026-08-01 09:24:44 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -796,7 +796,7 @@ const loginUser = asyncHandler(async(req,res)=>{
         try {
             const user = await User.findById(userId)
             const accessToken= user.generateAccessToken()
-            = user.generateRefreshToken()
+            const ref= user.generateRefreshToken()
             
         } catch (error) {
             throw new ApiError(500,"Something went wrong")
