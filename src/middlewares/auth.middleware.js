09024964d8a4be9ed91f5b01293b1765887347b2
Mudasir <apple.mudasir@gmail.com>
2026-08-01 09:49:48 +0700

    Auto commit

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -11,7 +11,7 @@ import { User } from "../models/user.models.js"
         if (!token) {
             throw new ApiError(401,"Cannot find the resh token")
         }
-        const decodedToken = jwt.verify(token,process.env.o)
+        const decodedToken = jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
     
         const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
     
