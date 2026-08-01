f78fba19ab6eecf05c237ccfe94e22a44eee6cb0
Mudasir <apple.mudasir@gmail.com>
2026-08-01 13:11:44 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -881,7 +881,7 @@ const changeCurrentPassword = asyncHandler( async(req,res)=>{
     const isPasswordCorrect= await user.isPasswordCorrect(oldPassword) //we have method in usermodel isPassswordCorrect
 
     if (!isPasswordCorrect) {
-        throw new ApiError(401,"")
+        throw new ApiError(401,"Invalid password")
         
     }
 
