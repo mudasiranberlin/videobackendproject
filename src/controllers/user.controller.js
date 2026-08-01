bd7027c7cb3fe0b96c4b3d1312219f154c263de2
Mudasir <apple.mudasir@gmail.com>
2026-08-01 13:11:27 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -881,6 +881,7 @@ const changeCurrentPassword = asyncHandler( async(req,res)=>{
     const isPasswordCorrect= await user.isPasswordCorrect(oldPassword) //we have method in usermodel isPassswordCorrect
 
     if (!isPasswordCorrect) {
+        throw new pass
         
     }
 
