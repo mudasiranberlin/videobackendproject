42817b483037e558654eb1837828e513056fcd32
Mudasir <apple.mudasir@gmail.com>
2026-08-02 09:11:02 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1359,6 +1359,7 @@ const updateUserAvatar = asyncHandler(async(req,res)=>{
         throw new ApiResponse(401,"File missing")  
     }
     const avatar = await uploadOnCloudinary(avatarlocalpath)
+    
 
 })
 
