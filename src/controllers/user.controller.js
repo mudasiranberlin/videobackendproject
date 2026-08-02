468cae4498564d9748e72a4b618a6f5273a9b70d
Mudasir <apple.mudasir@gmail.com>
2026-08-02 09:37:26 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1410,6 +1410,8 @@ const updateUserAvatar = asyncHandler(async(req,res)=>{
     if (!avatar.url) { // Suppose Cloudinary fails.
         throw new ApiResponse(401,"File missing") 
     }
+
+    Find the logged-in user.
     await findByIdAndUpdate(req.user?._id,
     {
         $set:{
