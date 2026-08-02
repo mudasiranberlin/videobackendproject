eff98087d0acd95e1d9d1e386aa3e0e79a2c5925
Mudasir <apple.mudasir@gmail.com>
2026-08-02 09:31:58 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1352,6 +1352,8 @@ const updateAccountDetails = asyncHandler(async(req,res)=>{
 
 })
 
+
+This function updates the user's profile picture (avatar).
 const updateUserAvatar = asyncHandler(async(req,res)=>{
 
     const avatarLocalPath = res.files?.path
