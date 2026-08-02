02ab5970f0bd103651744d0d0f19f6ed582e4223
Mudasir <apple.mudasir@gmail.com>
2026-08-02 11:14:19 +0700

    Auto commit

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {loginUser, logoutUser, registerUser,refreshAccessToken, changeCurrentPassword} from "../controllers/user.controller.js";
+import {loginUser, logoutUser, registerUser,refreshAccessToken, changeCurrentPassword, getCurrentUser} from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verfiyJWT } from "../middlewares/auth.middleware.js";
 
@@ -22,6 +22,6 @@ router.route("/register").post(
     router.route("/logout").post(verfiyJWT,logoutUser)
     router.route("/refresh-token").post(refreshAccessToken)
     router.route("/change-password").post(verfiyJWT,changeCurrentPassword)
-    router.route("/current-user").post(verfiyJWT,get)
+    router.route("/current-user").post(verfiyJWT,getCurrentUser)
 
 export default router
