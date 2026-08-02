b0070968f63b0d6c5a8d86a2b7a4ae05467a9d17
Mudasir <apple.mudasir@gmail.com>
2026-08-02 11:14:57 +0700

    Auto commit

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -23,6 +23,6 @@ router.route("/register").post(
     router.route("/refresh-token").post(refreshAccessToken)
     router.route("/change-password").post(verfiyJWT,changeCurrentPassword)
     router.route("/current-user").post(verfiyJWT,getCurrentUser)
-    router.route("/update-account").patch()
+    router.route("/update-account").patch(ver)
 
 export default router
