fd1cddffa4b57481f2c9c12ed1c9c3dec101c3fe
Mudasir <apple.mudasir@gmail.com>
2026-08-02 09:18:28 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1363,8 +1363,9 @@ const updateUserAvatar = asyncHandler(async(req,res)=>{
         throw new ApiResponse(401,"File missing") 
     }
     await findByIdAndUpdate(req.user?._id),
-    {},{
-        
+    {},
+    {
+
     }
 
 
