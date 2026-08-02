2e1816b42841c59a735523e67f4a55f966923642
Mudasir <apple.mudasir@gmail.com>
2026-08-02 09:32:20 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1354,6 +1354,20 @@ const updateAccountDetails = asyncHandler(async(req,res)=>{
 
 
 //  This function updates the user's profile picture (avatar)
+
+Think of it like this:
+
+User uploads image
+        ↓
+Server receives image
+        ↓
+Upload image to Cloudinary
+        ↓
+Get image URL
+        ↓
+Save URL in MongoDB
+        ↓
+Return updated user
 const updateUserAvatar = asyncHandler(async(req,res)=>{
 
     const avatarLocalPath = res.files?.path
