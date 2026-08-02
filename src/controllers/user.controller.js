9da3559fd2637f48b6b8adc50fad8c0cc44a5580
Mudasir <apple.mudasir@gmail.com>
2026-08-02 09:33:09 +0700

    Auto commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1372,11 +1372,11 @@ const updateAccountDetails = asyncHandler(async(req,res)=>{
 
 const updateUserAvatar = asyncHandler(async(req,res)=>{
 
-    Meaning
-We are creating a function named updateUserAvatar.
-Whenever a user uploads a new profile picture, this function runs.
-req = information sent by the user
-res = response sent back to the user
+//     Meaning
+// We are creating a function named updateUserAvatar.
+// Whenever a user uploads a new profile picture, this function runs.
+// req = information sent by the user
+// res = response sent back to the user
 
     const avatarLocalPath = res.files?.path
     if (!avatarLocalPath) {
